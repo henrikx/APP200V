b16ab64c79d8ad969096743db6da865bc9b43e99
MartinAndreHB <martinandrebraa@gmail.com>
2025-03-01 13:28:37 +0100

    Moved login to seperate document, styling the page roughly. Its kinda responsive. Buttons need some more work. Henrik added FireBase to the page.

---
+++ frontend/js/firebase.js
@@ -0,0 +1,36 @@
+  // Configuration fetched from firebase console
+  
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-login.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyB8NgzRkO7937ehuP-S8ABq7GEaSx69lVE",
+
+    authDomain: "app200v-gruppe-4.firebaseapp.com",
+
+    databaseURL: "https://app200v-gruppe-4-default-rtdb.europe-west1.firebasedatabase.app",
+
+    projectId: "app200v-gruppe-4",
+
+    storageBucket: "app200v-gruppe-4.firebasestorage.app",
+
+    messagingSenderId: "57768003349",
+
+    appId: "1:57768003349:web:de164e69824fa5c3920b1c"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
